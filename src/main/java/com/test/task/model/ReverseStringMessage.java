6cea59f350fce33d9261d6537fbbfc6c2ebdfe25
Karmanov Dmitry <karmanno@gmail.com>
2018-06-19 16:04:24 +1000

    Created websockets message model.

---
+++ src/main/java/com/test/task/model/ReverseStringMessage.java
@@ -0,0 +1,13 @@
+package com.test.task.model;
+
+public class ReverseStringMessage {
+    private String string;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}
