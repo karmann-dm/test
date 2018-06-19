7666d2479e978c236e9dd2d4ec01e1c6c57374e5
Karmanov Dmitry <karmanno@gmail.com>
2018-06-19 17:51:38 +1000

    Created event listener class for logging.

---
+++ src/main/java/com/test/task/listener/WebSocketEventListener.java
@@ -0,0 +1,23 @@
+package com.test.task.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class WebSocketEventListener {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        logger.info("New websocket connection!");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        logger.info("Websocket disconnected");
+    }
+}
