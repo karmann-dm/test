77a23dc315615e584a863f0a3403b9e2bc6a5b1b
Karmanov Dmitry <karmanno@gmail.com>
2018-06-20 18:38:43 +1000

    Added WordService to perform main functionality.

--- src/main/java/com/test/task/controller/MessageHandlingController.java
+++ src/main/java/com/test/task/controller/MessageHandlingController.java
@@ -1,19 +1,23 @@
 package com.test.task.controller;
 
+import com.test.task.model.InputMessage;
+import com.test.task.model.ReverseStringMessage;
 import com.test.task.model.Word;
+import com.test.task.service.WordService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Controller
 public class MessageHandlingController {
+    @Autowired
+    WordService wordService;
+
     @MessageMapping("/reverse-string")
-    @SendTo("/tr")
-    public List<Word> greeting(@Payload Word word) throws Exception {
-        return Arrays.asList(new Word("1"), new Word("2"));
+    @SendTo("/topic/tr")
+    public ReverseStringMessage greeting(InputMessage message) throws Exception {
+        wordService.addWord(new Word(message.getString()));
+        return new ReverseStringMessage(wordService.findAll());
     }
 }
