571e454012b1794978a8c724e21bac09aa6d206b
Karmanov Dmitry <karmanno@gmail.com>
2018-06-20 18:37:34 +1000

    Created simple InputMessage model.

---
+++ src/main/java/com/test/task/model/InputMessage.java
@@ -0,0 +1,18 @@
+package com.test.task.model;
+
+public class InputMessage {
+    private String string;
+
+    public InputMessage() {}
+    public InputMessage(String string) {
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+}
